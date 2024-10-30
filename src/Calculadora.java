778367a76cfea86c49edc3bff38174353adec873
marcolapcx <168039947+marcolapcx@users.noreply.github.com>
2024-10-30 20:09:46 -0300

    Swing

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,9 +1,43 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculadora {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton somarButton;
-    private JTextField textField3;
-    private JButton limparButton;
-}
+    private JTextField txtPrimeiroValor;
+    private JTextField txtSegundoValor;
+    private JButton btnSomar;
+    private JTextField txtResultado;
+    private JButton btnLimpar;
+    private JPanel panelMain;
+
+    public Calculadora() {
+
+        btnSomar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Peguei informação da tela
+                Integer primeiro = Integer.valueOf(txtPrimeiroValor.getText());
+                Integer segundo = Integer.valueOf(txtSegundoValor.getText());
+                // Processei
+                Integer soma = primeiro + segundo;
+
+                //Mandei para tela o resultado
+                txtResultado.setText(soma.toString());
+
+            }
+
+        });
+    }
+
+        public static void main (String[] args) {
+
+            JFrame frame = new JFrame("Minha Calculadora");
+            frame.setContentPane(new Calculadora().panelMain);
+            frame.setSize(400, 400);
+            frame.setVisible(true);
+
+
+        }
+
+    }
+
